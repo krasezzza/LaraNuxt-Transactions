df46d331666980f23ce3accb039ec0601a7a400e
Krasimir Tsvetanov <k.tsvetanov@next-it.bg>
2022-02-13 23:03:17 +0200

    Prepare default layout

---
+++ frontend/components/app/bar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar
+    id="app-bar"
+    fixed
+    app
+  >
+    <nuxt-link
+      to="/"
+      class="logo-link"
+    >
+      <div class="d-flex">
+        <v-img
+          :src="require('~/assets/peach.png')"
+          max-width="28"
+        />
+
+        <h2 class="ml-3">
+          Peachtree Bank
+        </h2>
+      </div>
+    </nuxt-link>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppBar'
+}
+</script>

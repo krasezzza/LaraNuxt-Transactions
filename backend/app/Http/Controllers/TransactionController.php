7c4286ad9e63ef5729f1a51a24bd10e63a52cb55
Krasimir Tsvetanov <k.tsvetanov@next-it.bg>
2022-02-15 23:04:38 +0200

    Create backend structure for basic API usage

---
+++ backend/app/Http/Controllers/TransactionController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Database\Eloquent\Collection;
+
+class TransactionController extends Controller
+{
+    /**
+     * Get a single transaction.
+     *
+     * @param Integer $id
+     */
+    public function getOne($id)
+    {
+        return Transaction::find($id);
+    }
+
+    /**
+     * Collect a listing of transactions.
+     *
+     * @return Collection|Transaction[]
+     */
+    public function listAll()
+    {
+        return Transaction::all();
+    }
+
+    /**
+     * Store a newly created transaction.
+     *
+     * @param Request $request
+     */
+    public function create(Request $request)
+    {
+        $request->validate([
+            'sender' => 'required',
+            'receiver' => 'required',
+            'amount' => 'required'
+        ]);
+
+        return Transaction::create($request->all());
+    }
+
+    /**
+     * Update an already created transaction.
+     *
+     * @param Request $request
+     * @param Integer $id
+     */
+    public function update(Request $request, $id)
+    {
+        $transaction = Transaction::find($id);
+        $transaction->update($request->all());
+
+        return $transaction;
+    }
+
+    /**
+     * Delete an already created transaction.
+     *
+     * @param Integer $id
+     */
+    public function delete($id)
+    {
+        return Transaction::destroy($id);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/transactions', [TransactionController::class, 'listAll']);
+Route::get('/transactions/{id}', [TransactionController::class, 'getOne']);
+Route::post('/transactions', [TransactionController::class, 'create']);
+Route::put('/transactions/{id}', [TransactionController::class, 'update']);
+Route::delete('/transactions/{id}', [TransactionController::class, 'delete']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
